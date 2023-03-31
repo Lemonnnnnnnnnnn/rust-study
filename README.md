bd2d40081309b4c2e5c74707ef44f8fdad0cd6cb
soul <104170577@qq.com>
2023-03-31 15:05:47 +0000

    feat: guess_number

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 - Cargo 有一个很棒的功能是：运行 cargo doc −−open 命令来构建所有本地依赖提供的文档，并在浏览器中打开。
 - 每个包叫做一个crate.
 - trait 是一个关键的名词，它看起来像是一种依赖，引入依赖才可以调用crate中的方法。
+- Result ： 
+  - 一个结果，可能错误，可以调用.expect方法
+  - 可以用Result{Ok() => {} , Err() => {}} 对错误和正确的结果进行处理
+  - Result{} 中可以调用任意的枚举值来对结果进行处理，这使它起到了swich的作用
+- u32 : 无符号32位整数，i32：有符号32位整数
+- let mut a + let a : 隐藏可变值a
+- match 与 switch 的异同点
+  - match返回Result
+- Ordering 枚举类型
+  - js的枚举类：Ordering.a
+  - rust的枚举类：Ordering::a
+- 所有的格式转换都用 parse() 来做，依靠自动推断
+- loop = while(1)
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess");
 
-    io::stdin()
-    // read_line: 无论用户在标准输入中键入什么内容，都将其追加（不会覆盖其原有内容）到一个字符串中，
-    // 因此它需要字符串作为参数
-        .read_line(&mut guess) 
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed : {guess}");
-    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // let guess: u32 = guess.trim().parse().expect("please type a number");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_)=> {
+                println!("please input number");
+                continue
+            } ,
+        };
+
+        println!("You guessed : {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } ,
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 }
